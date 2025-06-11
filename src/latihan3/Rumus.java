bdf0de0d3bfc01484a60b15c7c076e83468e68e4
hasnia3107 <hasnianiu@gmail.com>
2025-06-11 16:07:53 +0800

    membuat accessor dan construktor

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -6,10 +6,22 @@ public class Rumus {
     //Kelas : 4D REGULER BJB
     int sisi, panjang, lebar, hasil;
     
+    public Rumus() {
+        sisi = 0;
+        panjang = 0;
+        lebar = 0;
+        hasil = 0;
+    }
+    
     public void hitungLuasPersegi() {
         hasil = sisi * sisi;
+        System.out.println("Luas Persegi = " + hasil);
     }
     public void hitungLuasPersegiPanjang() {
         hasil = panjang * lebar;
+        System.out.println("Luas Persegi Panjang = " + hasil);
+    }
+    public int getHasil() {
+        return hasil;
     }
 }
